3418ec7b40189d23914ff02a3b8af52d8bec9ff3
Daniel Fróes <daniellfroes@gmail.com>
2025-05-15 11:16:10 -0300

    new logo component, adjust imports

--- site-blog/src/components/layout/layout.tsx
+++ site-blog/src/components/layout/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "../footer/footer";
-import Header from "../header/header";
+import { Footer } from "../footer";
+import { Header } from "../header";
 
 type LayoutProps = {
   children: React.ReactNode;
