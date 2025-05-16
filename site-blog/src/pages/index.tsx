29e4f201750caad35973988c54e6337a9705e22d
Daniel Fróes <daniellfroes@gmail.com>
2025-05-16 14:37:47 -0300

    create feature section on  landing page

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/components/hero-section";
+import { Features } from "@/components/feature";
 
 export default function Home() {
   return (
     <>
       <article className="flex flex-col">
         <HeroSection />
+        <Features />
       </article>
     </>
   );
