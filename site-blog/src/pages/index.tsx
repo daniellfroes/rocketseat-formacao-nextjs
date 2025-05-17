f6708de43a54d9fe417c143ee9235e0ed6ac2c93
Daniel Fróes <daniellfroes@gmail.com>
2025-05-17 13:25:59 -0300

    refactor landing page structure, folder location and exports

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,19 +1,5 @@
-import { HeroSection } from "@/components/hero-section";
-import { FeaturesSection } from "@/components/feature-section";
-import { SupportSection } from "@/components/support-section";
-import { CustomerStorySection } from "@/components/customer-story-section";
-import { CallToActionSection } from "@/components/call-to-action-section";
+import { LandingPage } from "@/templates/landing-page";
 
 export default function Home() {
-  return (
-    <>
-      <article className="flex flex-col">
-        <HeroSection />
-        <FeaturesSection />
-        <SupportSection />
-        <CustomerStorySection />
-        <CallToActionSection />
-      </article>
-    </>
-  );
+  return <LandingPage />;
 }
