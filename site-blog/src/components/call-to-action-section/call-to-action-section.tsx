845d2189019107c6b52c7dca1e8feeba2efe0411
Daniel Fróes <daniellfroes@gmail.com>
2025-05-17 13:10:09 -0300

    add background images on call to action and support sections

--- site-blog/src/components/call-to-action-section/call-to-action-section.tsx
+++ site-blog/src/components/call-to-action-section/call-to-action-section.tsx
@@ -4,8 +4,10 @@ import { ArrowRight, Store } from "lucide-react";
 
 export function CallToActionSection() {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90"></div>
+
+      <div className="relative container">
         <div className="flex flex-col items-center gap-6 text-center">
           <div className="p-4 bg-cyan-300 w-fit rounded-full">
             <Store className="text-cyan-100" />
