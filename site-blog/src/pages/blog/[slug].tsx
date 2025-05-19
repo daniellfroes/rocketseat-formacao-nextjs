98f654badf0cdfa90acd1944f3dfad20076d3584
Daniel Fróes <daniellfroes@gmail.com>
2025-05-18 23:33:51 -0300

    create aside share component

--- site-blog/src/pages/blog/[slug].tsx
+++ site-blog/src/pages/blog/[slug].tsx
@@ -11,6 +11,8 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Avatar } from "@/components/avatar";
+import { Markdown } from "@/components/markdown";
+import { Button } from "@/components/ui/button";
 
 export default function PostPage() {
   const router = useRouter();
@@ -24,52 +26,84 @@ export default function PostPage() {
 
   return (
     <main className="mt-32 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href={"/blog"}>Blog</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+      <div className="container space-y-12 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href={"/blog"}>Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
 
-          <BreadcrumbSeparator />
+            <BreadcrumbSeparator />
 
-          <BreadcrumbItem>
-            <span className=" text-blue-300 text-action-sm">{post?.title}</span>{" "}
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+            <BreadcrumbItem>
+              <span className=" text-blue-300 text-action-sm">
+                {post?.title}
+              </span>{" "}
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
 
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
-        <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
-          <figure className="relative aspect-[16/10] w-full overflow-hidden runded-lg">
-            <Image
-              src={post?.image}
-              alt={post?.title}
-              fill
-              className="object-cover"
-            />
-          </figure>
-
-          <header className="p-4 md:p-6 lg:p-12 pb-0">
-            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl">
-              {post?.title}
-            </h1>
-            <Avatar.Container>
-              <Avatar.Image
-                src={post?.author.avatar.trimEnd()}
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+          <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden runded-lg">
+              <Image
+                src={post?.image}
                 alt={post?.title}
+                fill
+                className="object-cover"
               />
-              <Avatar.Content>
-                <Avatar.Title>{post?.author.name}</Avatar.Title>
-                <Avatar.Description>
-                  Publicado em{" "}
-                  <time dateTime={post?.date}>{publishedDate}</time>
-                </Avatar.Description>
-              </Avatar.Content>
-            </Avatar.Container>
-          </header>
-        </article>
+            </figure>
+
+            <header className="p-4 md:p-6 lg:p-12 pb-0 mt-8 md:mt-12">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl">
+                {post?.title}
+              </h1>
+              <Avatar.Container>
+                <Avatar.Image
+                  src={post?.author.avatar.trimEnd()}
+                  alt={post?.title}
+                  size="sm"
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em{" "}
+                    <time dateTime={post?.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+
+            <div className="prose prove-invert max-w-none px-4 mt-12 md:px-6 lg:px-12">
+              <Markdown content={post.body.raw} />
+            </div>
+          </article>
+
+          <aside className="space-y-6">
+            <div className="rounded-lg bg-gray-700 p-4 md:p-6">
+              <h2 className="mb-4 text-heading-xs text-gray-100">
+                Compartilhar
+              </h2>
+
+              <div className="space-y-3">
+                {[
+                  { key: 1, providerName: "Linkedin" },
+                  { key: 2, providerName: "Facebook" },
+                ].map((provider) => (
+                  <Button
+                    key={provider.key}
+                    variant={"outline"}
+                    className="w-full justify-start gap-2"
+                  >
+                    {provider.providerName}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </aside>
+        </div>
       </div>
     </main>
   );
