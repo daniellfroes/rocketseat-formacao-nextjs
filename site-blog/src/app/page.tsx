17c81c46977371bddddbc56d1f61215f2eabd498
Daniel Fróes <daniellfroes@gmail.com>
2025-05-20 14:53:12 -0300

    chroe: add seo in landing page

--- site-blog/src/app/page.tsx
+++ site-blog/src/app/page.tsx
@@ -1,4 +1,25 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda os seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda os seus produtos como afiliado em um único lugar",
+    url: "https://rocketseat-formacao-nextjs.vercel.app/og-image.jpg",
+    siteName: "Site.set",
+    locale: "pt-BR",
+    type: "website",
+    images: {
+      url: "https://rocketseat-formacao-nextjs.vercel.app/og-image.jpg",
+      alt: "Site.set",
+      height: 600,
+      width: 800,
+    },
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
